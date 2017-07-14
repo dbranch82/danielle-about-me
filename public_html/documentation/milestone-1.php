e9cd3a6e9b47d8d6b120fcf39d322ce14e9894af
Danielle Branch <dbranch82@gmail.com>
2017-07-14 09:56:07 -0600

    Last edits done

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,14 +10,14 @@
 		<h1>Persona</h1>
 		<p><strong>Name:</strong> Sara Smith</p>
 		<p><strong>Age:</strong> 32</p>
-		<p><strong>Profession :</strong>Sara owns a catering business it is still in its early years but the company is starting to get
+		<p><strong>Profession :</strong> Sara owns a catering business it is still in its early years but the company is starting to get
 			busy.
 			They are looking to hire a Web Development company to make and manage their website. The company markets to
 			customers needing
 			specific dietary modifications.</p>
 		<p><strong>Technology:</strong> Uses windows 8 probook when she is dealing with business or in office and a newer Samsung 8 for
 			day to day life.</p>
-		<p><strong>Attitudes and Behaviors:</strong> Sara is fully dedicated to her four children and her business. Saras' main focus
+		<p><strong>Attitudes and Behaviors:</strong> Sara is fully dedicated to her four children and her business. Sara's main focus
 		is on her children and her business she will spend as much time as needed to find the right fit for her company. She is looking
 		for a personal connection. Experience is as importsnt as character to her.</p>
 		<p><strong>Frustrations and Needs:</strong> Sara is looking to hire someone who understands her as a person and her vision
