4d0f937630013ee992e0de9545e83388687bd106
Danielle Branch <dbranch82@gmail.com>
2017-07-14 08:23:53 -0600

    Added picture correctly

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,24 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>About Danielle</title>
+		<title>About Sara</title>
 	</head>
 	<body>
-		<h1 id="page header">Meet Danielle</h1>
+		<h1 id="page header">Meet Sara</h1>
+		<img src="images/sarapic.jpg" alt="sara picture"/>
+		<h1>Persona</h1>
+		<p><strong>Name:</strong> Sara Smith</p>
+		<p><strong>Age:</strong> 32</p>
+		<p><strong>Profession :</strong>Sara owns a catering business it is still in its early years but the company is starting to get
+			busy.
+			They are looking to hire a Web Development company to make and manage their website. The company markets to
+			customers needing
+			specific dietary modifications.</p>
+		<p><strong>Technology:</strong> Uses windows 8 probook when she is dealing with business or in office and a newer Samsung 8 for
+			day to day life.</p>
+		<p><strong>Attitudes and Behaviors:</strong>Sara is fully dedicated to her four children and her business. Saras' main focus
+		is on her children and her business she will spend as much time as needed to find the right fit for her company. She is looking
+		for a personal connection. Experience is as importsnt as character to her.</p>
+		<p><strong>Frustrations and Needs:</strong></p>
 	</body>
 </html>
