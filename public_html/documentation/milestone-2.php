856ceb9a04eb5487919093c7b30cae0f1591caa3
Danielle Branch <dbranch82@gmail.com>
2017-07-17 13:39:07 -0600

    Made basic outline for milestone 2, set different sections to bold

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Content Strategy</title>
+		<title>milestone-2</title>
 	</head>
 	<body>
-		<h1></h1>
+		<h1><strong>Content</strong></h1>
+		<p>I want to use one picture as a focal point to highlight what I do and who I am.</p>
+		<p>I would like to use my photo inside my main photo as a link to reach the text of my about me.</p>
+		<p>I want to use links to lead clients to my social networks</p>
+		<h1><strong>Purpose and Strategy</strong></h1>
+		<p>I think a simple user experience with something like a photo inside a photo and links to my github and linkedin will
+		let my work speak for itself.</p>
+		<p>I think as my company will have a focus on charity that my about me needs to also include personal getting to know me info
+		that way my clients can feel some sort of connection to the person they will be partnering with.</p>
 	</body>
 </html>
