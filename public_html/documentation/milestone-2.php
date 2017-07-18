cff46e87abc9ea98ed4965c9080c830a0b116433
Danielle Branch <dbranch82@gmail.com>
2017-07-18 10:39:46 -0600

    Rerefixed with no inline css

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,25 +2,26 @@
 <html>
 	<head>
 		<title>milestone-2</title>
+
 	</head>
 	<body>
 		<h1><strong>Content Strategy</strong></h1>
 		<p>My site will consist of two pages</p>
 		<p>The first page will consist of two sections</p>
+			<p>Welcome</p>
 		<ul>
-			<li>Welcome</li>
-			<li style="list-style: none;">A small introduction that clearly defines my role within my company</li>
+			<li>A small introduction that clearly defines my role within my company</li>
 			<li>Portfolio</li>
-			<li style="list-style: none;" >A showcase of my professional work and the work I have done for charity within my company, it will list under each project
+			<li>A showcase of my professional work and the work I have done for charity within my company, it will list under each project
 			what technology I used for each project</li>
 		</ul>
 		<p>The second page will also consist of two sections</p>
-		<ul>
-			<li>About me</li>
-			<li style="list-style: none;" >A more indepth bio that explains not only who I am as a developer but also who I am as a person. I think that the
+			<p>About me</p>
+			<ul>
+			<li>A more indepth bio that explains not only who I am as a developer but also who I am as a person. I think that the
 			whole about me section should be really casual as to build a relationship with the user</li>
 			<li>Contact Me</li>
-			<li style="list-style: none;">A small contact form that will basically just link back to the company website. I will also have icons of my social
+			<li>A small contact form that will basically just link back to the company website. I will also have icons of my social
 			media sites. </li>
 		</ul>
 	</body>
