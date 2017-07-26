5942aee605a4d49b42abaf04efb83a105ab3b446
dbranch6 <dbranch6@cnm.edu>
2017-07-26 10:07:19 -0600

    formatted code

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,26 +6,32 @@
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
-		<p>My site will be a way for my customers to get to know who I am as a developer. While having a way to see my work visually.</p>
-			<h1>Welcome Section</h1>
-		<p>This will have an image of myself with my name and titles. There will also be a menu button in the top left corner
-		that will allow users to instantly jump to specific sections.</p>
+		<p>My site will be a way for my customers to get to know who I am as a developer. While having a way to see my
+			work visually.</p>
+		<h1>Welcome Section</h1>
+		<p>This will have an image of myself with my name and titles. There will also be a menu button in the top left
+			corner
+			that will allow users to instantly jump to specific sections.</p>
 		<h1>The About Me section</h1>
 		<p>A more indepth section that is seperated into three paragraphs. This will be set up in an F pattern. There will
-		also be an up arrow in bottom right corner allowing user to return to Welcome section. These three paragraphs will
-		be a light getting to know me because I would like to partner with clients that are a good fit for me and my business.
+			also be an up arrow in bottom right corner allowing user to return to Welcome section. These three paragraphs
+			will
+			be a light getting to know me because I would like to partner with clients that are a good fit for me and my
+			business.
 		<ul>
-				<li>As a Developer</li>
-				<li>As a business owner</li>
+			<li>As a Developer</li>
+			<li>As a business owner</li>
 			<li>As a person</li>
 		</ul>
 		<h1>The Testimonial/Project section</h1>
-		<p>This section will have pictures of my work with a small blurb underneath with comments from clients I have partnered with. There will
-		also be an up arrow in bottom right hand corner allowing user to return to Welcome Page.</p>
+		<p>This section will have pictures of my work with a small blurb underneath with comments from clients I have
+			partnered with. There will
+			also be an up arrow in bottom right hand corner allowing user to return to Welcome Page.</p>
 		<h1>The Contact me section</h1>
 		<p>This section will have a small "I'd love to work with you blurb" along side a simple contact me form.
-		At the bottom of this page will have links to my social media and also a phone icon for people that would prefer
-		direct contact.</p>
+			At the bottom of this page will have links to my social media and also a phone icon for people that would
+			prefer
+			direct contact.</p>
 		<h2>Web wireframe</h2>
 		<img style="margin:0 auto;display: block" src="images/page1wireframe.png" alt="page 1 web wireframe"/>
 		<img style="margin:0 auto;display: block" src="images/page2wireframe.png" alt="page 2 web wireframe"/>
