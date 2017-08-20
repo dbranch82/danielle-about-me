ceac1bd3f351a8a9ec5bec9d2fd23257163082df
Danielle Branch <dbranch82@gmail.com>
2017-08-19 18:54:27 -0600

    fixing everything

--- public_html/index.php
+++ public_html/index.php
@@ -2,20 +2,14 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-
-		<!-- custom stylesheet -->
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Playball|Roboto:400,400i,700,700i" rel="stylesheet">
 
+		<!-- custom stylesheet -->
+		<link rel="stylesheet" href="css/style.css">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,27 +22,18 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-		<title>Name of my website</title>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title></title>
 	</head>
 	<body>
-		<div class="clr"></div>
-
-		<h1> <span>Our Company</span></h1>
-		<div class="content">
-			<ul class="bmenu">
-				<li><a href="#">Our Company</a></li>
-				<li><a href="#">Pricing</a></li>
-				<li><a href="#">Who we are</a></li>
-				<li><a href="#">Personal Projects</a></li>
-				<li><a href="#">Contact Us</a></li>
-			</ul>
-
-				This text appears in front of the background image.
+		<h1></h1>
+		<div class="jumbotron">
+			<div class="container">
+				<h1>My Company Name</h1>
+				<p>...</p>
 			</div>
-
+		</div>
 
 	</body>
-	</html>
+</html>
