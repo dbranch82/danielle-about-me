7d9478b3d6bf08a351a143e0847fbd3e86bb2229
Danielle Branch <dbranch82@gmail.com>
2017-08-21 08:52:34 -0600

    trying out parallax

--- public_html/index.php
+++ public_html/index.php
@@ -2,38 +2,88 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<!-- Bootstrap Core CSS -->
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Playball|Roboto:400,400i,700,700i" rel="stylesheet">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<!-- custom stylesheet -->
-		<link rel="stylesheet" href="css/style.css">
+		<!-- Theme CSS -->
+		<link href="css/style.css" rel="stylesheet">
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
-		<!-- jQuery -->
+		<!-- jQuery - reqiured for Bootstrap Components -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
-		<title></title>
-	</head>
 	<body>
-		<h1></h1>
-		<div class="jumbotron">
-			<div class="container">
-				<h1>My Company Name</h1>
-				<p>...</p>
+		<header></header>
+		<div class="bgimg-1">
+			<div class="caption">
+				<span class="border">SCROLL DOWN</span>
 			</div>
 		</div>
 
+		<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+			<h3 style="text-align:center;">Parallax Demo</h3>
+			<p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+		</div>
+
+		<div class="bgimg-2">
+			<div class="caption">
+				<span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>
+			</div>
+		</div>
+
+		<div style="position:relative;">
+			<div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+				<p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+			</div>
+		</div>
+
+		<div class="bgimg-3">
+			<div class="caption">
+				<span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">SCROLL UP</span>
+			</div>
+		</div>
+
+		<div style="position:relative;">
+			<div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+				<p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+			</div>
+		</div>
+
+		<div class="bgimg-1">
+			<div class="caption">
+				<span class="border">COOL!</span>
+			</div>
+		</div>
+		<div class="parallax"></div>
+
+
+		<div class="section2">
+
+		</div>
+		<div class="section3">
+
+		</div>
+		<div class="section4">
+
+		</div>
+		<div class="contactform">
+
+		</div>
 	</body>
 </html>
