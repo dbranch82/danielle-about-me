73506ee1b8906e6e8f6b7d1e64046f12dc5a2ca7
Danielle Branch <dbranch82@gmail.com>
2017-08-23 11:07:51 -0600

    comment for contact box

--- public_html/index.php
+++ public_html/index.php
@@ -12,6 +12,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
 		<!-- Theme CSS -->
 		<link href="css/style.css" rel="stylesheet">
 
@@ -34,7 +36,8 @@
 			<nav id="header" class="navbar navbar-fixed-top">
 				<div id="header-container" class="container navbar-container">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+								  aria-expanded="false" aria-controls="navbar">
 							<span class="sr-only">Toggle navigation</span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -57,11 +60,14 @@
 				<p></p>
 			</div>
 		</div>
-<!-- Why the weird white line -->
+		<!-- Why the weird white line -->
 
 		<div class="divstyle1">
 			<p>Our Story</p>
-			<p>Michelle and Danielle met the summer of 2017 while taking classes in computer programming. The instantly connected and became friends. They realized that they both had strong people skills and were both confident in different areas of web development and design. They developed their business plan while still in school and the day of graduation launched Unisus</p>
+			<p>Michelle and Danielle met the summer of 2017 while taking classes in computer programming. The instantly
+				connected and became friends. They realized that they both had strong people skills and were both confident
+				in different areas of web development and design. They developed their business plan while still in school
+				and the day of graduation launched Unisus</p>
 		</div>
 		<div class="bgimg-2">
 			<div class="caption">
@@ -69,7 +75,10 @@
 			</div>
 		</div>
 		<div class="divstyle1">
-			<p>We believe in giving back whenever possible. Part of our business model includes giving back to the community through job development tools. We believe in what comes around goes around so we want to spread give back some of the oppurtunity that we have been fortunate enough to receive.</p>
+			<p>We are a 100% women owned business. We believe in giving back whenever possible. Part of our business model
+				includes giving back to the community through job development tools. We believe in what comes around goes
+				around so we want to spread give back some of the oppurtunity that we have been fortunate enough to
+				receive.</p>
 		</div>
 
 		<div class="bgimg-4">
@@ -79,9 +88,12 @@
 		</div>
 
 		<div class="divstyle2">
-				<p>We are a 100% women owned business. We believe in opportunity and a big part of our business model is giving back to our community. We build and design websites for small business but we also do one pagers for people needing something more to add to their business cards.</p>
+			<p> If your looking for a one page portfolio or a full blown custom built website we have you covered. We build
+				and design websites for small business but we also do one pagers for people needing something more to add to
+				their business cards. Regardless of your needs we will let the project talk to us and work together to help
+				you achieve your vision.</p>
 
-			</div>
+		</div>
 
 		<div class="bgimg-3">
 			<div class="caption">
@@ -105,30 +117,31 @@
 								<div class="well text-center">
 									<p>Full Site</p>
 
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
 
-		<div style="position:relative;">
-			<div class="oops">
-				<p>Projects we have worked or are currently working on</p>
-				<p>Ask Rochelle about bubbles need at least four circles here for projects.</p>
-			</div>
-		</div>
+				<div style="position:relative;">
+					<div class="oops">
+						<p>Projects we have worked or are currently working on</p>
+						<p>Ask Rochelle about bubbles need at least four circles here for projects.</p>
+					</div>
+				</div>
 
 
-			<div class="caption">
-				<span class="border">Let's Talk!</span>
-				<p>Contact Form</p>
-				<footer><a href="tel:">505-589-1515</a></footer>
+				<div class="caption">
+					<span class="border">Let's Talk!</span>
+					<p>Contact Form</p>
+					<footer><a href="tel:">505-589-1515</a></footer>
+				</div>
 			</div>
-		</div>
 			<!--I want this in a box-->
 			<section id="contact">
 				<div class="section-content">
-					<h1 class="section-header"><span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> </span></h1>
+					<h1 class="section-header"><span class="content-header wow fadeIn " data-wow-delay="0.2s"
+																data-wow-duration="2s"> </span></h1>
 					<h3>General questions / Project inquires</h3>
 				</div>
 				<div class="contact-section">
@@ -141,22 +154,41 @@
 								</div>
 								<div class="form-group">
 									<label for="exampleInputEmail">Email Address</label>
-									<input type="email" class="form-control" id="exampleInputEmail" placeholder=" Enter Email id">
+									<input type="email" class="form-control" id="exampleInputEmail"
+											 placeholder=" Enter Email id">
 								</div>
 								<div class="form-group">
 									<label for="telephone">Mobile No.</label>
-									<input type="tel" class="form-control" id="telephone" placeholder=" Enter 10-digit mobile no.">
+									<input type="tel" class="form-control" id="telephone"
+											 placeholder=" Enter 10-digit mobile no.">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for ="description"> Message</label>
-									<textarea  class="form-control" id="description" placeholder="Enter Your Message"></textarea>
+									<label for="description"> Message</label>
+									<textarea class="form-control" id="description" placeholder="Enter Your Message"></textarea>
 								</div>
 								<div>
 
-									<button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>  Send Message</button>
+									<button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane"
+																											  aria-hidden="true"></i> Send Message
+									</button>
 								</div>
+								<footer>
+									<div class="col-md-12">
+										<ul class="social-network social-circle">
+											<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+											<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+											</li>
+											<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+											</li>
+											<li><a href="#" class="icoGoogle" title="Google +"><i
+														class="fa fa-google-plus"></i></a></li>
+											<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+											</li>
+										</ul>
+									</div>
+								</footer>
 
 							</div>
 						</form>
