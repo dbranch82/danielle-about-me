76d3d524e20233a0c97a5046ceedd93e8c0a4084
Danielle Branch <dbranch82@gmail.com>
2017-08-23 12:02:39 -0600

    added javascript

--- public_html/index.php
+++ public_html/index.php
@@ -96,14 +96,14 @@
 
 		</div>
 		<div class="container">
-			<h2>Normal</h2>
+			<h2>What we provide</h2>
 			<div class="row form-group product-chooser">
 
 				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-					<div class="product-chooser-item selected">
+					<div class="product-chooser-item">
 						<img src="http://renswijnmalen.nl/bootstrap/desktop_mobile.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile and Desktop">
 						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-							<span class="title">Mobile and Desktop</span>
+							<span class="title">Full Website build</span>
 							<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
 							<input type="radio" name="product" value="mobile_desktop" checked="checked">
 						</div>
@@ -115,7 +115,7 @@
 					<div class="product-chooser-item">
 						<img src="http://renswijnmalen.nl/bootstrap/desktop.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Desktop">
 						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-							<span class="title">Desktop</span>
+							<span class="title">Wordpress</span>
 							<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
 							<input type="radio" name="product" value="desktop">
 						</div>
@@ -127,7 +127,7 @@
 					<div class="product-chooser-item">
 						<img src="http://renswijnmalen.nl/bootstrap/mobile.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile">
 						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-							<span class="title">Mobile</span>
+							<span class="title">One page portfolios or scrolling websites</span>
 							<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
 							<input type="radio" name="product" value="mobile">
 						</div>
@@ -212,6 +212,6 @@
 						</div>
 				</section>
 
-			</div>
+		<script src="javascript/javascript.js"></script>
 	</body>
 </html>
