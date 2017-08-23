7e115838f67acb40332d7e25f9671c2ef2e8d564
Danielle Branch <dbranch82@gmail.com>
2017-08-23 10:13:39 -0600

    checking if javascript file works on nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -27,7 +27,29 @@
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
 
+
 	<body>
+		<header><!-- Fixed navbar -->
+			<nav id="header" class="navbar navbar-fixed-top">
+				<div id="header-container" class="container navbar-container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a id="brand" class="navbar-brand" href="#">Project name</a>
+					</div>
+					<div id="navbar" class="collapse navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+					</div><!-- /.nav-collapse -->
+				</div><!-- /.container -->
+			</nav><!-- /.navbar --></header>
 		<div class="bgimg-1">
 			<div class="caption">
 				<span class="border">Whatever our name is</span>
@@ -143,5 +165,6 @@
 			</section>
 
 		</div>
+		<script src="javascript/javascript.js"></script>
 	</body>
 </html>
