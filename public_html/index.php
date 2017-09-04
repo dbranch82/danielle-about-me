124be8d56b0d206cd24f00b5e76bf7d8424ada28
Danielle Branch <dbranch82@gmail.com>
2017-09-03 22:52:20 -0600

    ran composer install

--- public_html/index.php
+++ public_html/index.php
@@ -39,6 +39,11 @@
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 		<script type="text/javascript"
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 	<body>
 
