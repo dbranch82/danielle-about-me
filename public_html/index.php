59441c6f97971f9849c63e862d9de43685d7e6b1
Danielle Branch <dbranch82@gmail.com>
2017-09-03 22:05:10 -0600

    added meet team section fixed navbar added pics to product section

--- public_html/index.php
+++ public_html/index.php
@@ -42,44 +42,28 @@
 
 	<body>
 
-		<!-- Fixed navbar -->
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="container">
+		<nav class="navbar navbar-inverse">
 				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-							  aria-expanded="false" aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Unisis</a>
+					<a class="navbar-brand" href="voicebox.html">VoiceBOX</a>
 				</div>
-				<div id="navbar" class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">Services</a></li>
-						<li><a href="#contact">Contact</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
+				<ul class="nav navbar-nav">
+					<li><a href="#">TOUR</a></li>
+					<li><a href="#">HOW TO PODCAST</a></li>
+					<li><a href="#">OUR PODCASTS</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				</ul>
 		</nav>
 
 		<!-- Begin page content -->
-		<div class="container">
-			<div class="page-header">
-			</div>
 			<div class="bgimg-1">
 				<div class="caption">
 					<span class="border"></span>
 					<p>Our Story</p>
-					<p>Michelle and Danielle met the summer of 2017 while taking classes in computer programming. They
-						instantly
-						connected and became friends. They realized that they both had strong people skills and were both
-						confident
-						in different areas of web development and design. They developed their business plan while still in
-						school
-						and the day of graduation launched Unisus</p>
+					<p>We are an Albuquerque company specializing in whatever the hell we do.
+					</p>
+					<p>We work with Php, CSS, Bootstrap, javascript, and Wordpress. </p>
+					<p>We partner with other artisans in the albuquerque area to help you vision become a reality.</p>
 					<p></p>
 				</div>
 			</div>
@@ -91,10 +75,10 @@
 
 					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 						<div class="product-chooser-item">
-							<img src="http://renswijnmalen.nl/bootstrap/desktop_mobile.png"
+							<img src="images/css.jpg"
 								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile and Desktop">
 							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-								<span class="title">Full Website build</span>
+								<span class="title">CSS fixes</span>
 								<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
 								<input type="radio" name="product" value="mobile_desktop" checked="checked">
 							</div>
@@ -104,7 +88,7 @@
 
 					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 						<div class="product-chooser-item">
-							<img src="http://renswijnmalen.nl/bootstrap/desktop.png"
+							<img src="images/wordpress.jpg"
 								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Desktop">
 							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
 								<span class="title">Wordpress</span>
@@ -114,13 +98,12 @@
 							<div class="clear"></div>
 						</div>
 					</div>
-
 					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 						<div class="product-chooser-item">
-							<img src="http://renswijnmalen.nl/bootstrap/mobile.png"
+							<img src="images/fullwebdesign.png"
 								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile">
 							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-								<span class="title">One page portfolios or scrolling websites</span>
+								<span class="title">Fully responsive websites</span>
 								<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
 								<input type="radio" name="product" value="mobile">
 							</div>
@@ -128,82 +111,105 @@
 						</div>
 					</div>
 				</div>
-
-
-				<!--I SHOULD HAVE DONE A MOCK UP-->
-					<form id="contact-form" action="php/mailer.php" method="post">
-						<div class="form-group">
-							<label for="name">Name <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-user" aria-hidden="true"></i>
-								</div>
-								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="email">Email <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-envelope" aria-hidden="true"></i>
-								</div>
-								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="subject">Subject</label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-pencil" aria-hidden="true"></i>
-								</div>
-								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="message">Message <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-comment" aria-hidden="true"></i>
-								</div>
-								<textarea class="form-control" rows="5" id="message" name="message"
-											 placeholder="Message (2000 characters max)"></textarea>
-							</div>
-						</div>
-
-						<!-- reCAPTCHA -->
-						<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
-
-						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-					</form>
-					<div class="col-xs-12">
-						<div id="output-area"></div>
-						<!--end form-->
-						<footer>
-							<div class="col-md-12">
-								<ul class="social-network social-circle">
-									<li><a href="#" class="icoGithub" title="Github"><i class="fa fa-github"></i></a>
-									</li>
-									<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
-									</li>
-									<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
-									</li>
-									<li><a href="#" class="icoInstagram" title="Google +"><i
-												class="fa fa-instagram"></i></a></li>
-									<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-									</li>
-								</ul>
-							</div>
-						</footer>
-					</div>
-					<a href="tel:">505-589-1515</a>
-					</section>
-					<footer class="footer">
+			</div>
+<!---us--->
+					<div class="section">
 						<div class="container">
-							<p class="text-muted">WhateverWhatever</p>
-						</div>
-					</footer>
+							<div class="row">
+								<div class="col-md-12">
+									<h1 class="text-center text-primary">Meet our Team</h1>
+									<p class="text-center">We are a group of professionals that use our individual skill sets to deliver polished, efficiant websites </p>
+								</div>
+							</div>
+							<div class="row">
 
-					<script src="javascript/javascript.js"></script>
+
+								<div class="col-md-4">
+									<img src="images/michelle.jpg"
+										  class="img-quadrata center-block" >
+									<h3 class="text-center" >Michelle Allen
+										<br/>
+										<small> Web Designer/VP Sales</small></h3>
+								</div>
+								<div class="col-md-4">
+									<img src="images/myface.jpg"
+										  class="center-block img-quadrata" >
+									<h3 class="text-center" >Danielle Branch
+										<br/>
+										<small> Web Designer/VP Networking</small></h3>
+								</div>
+								<div class="col-md-4">
+									<img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?q=75&fm=jpg&s=cfeadbd7a991e58b553bee29a7eeca55"
+										  class="center-block img-quadrata">
+									<h3 class="text-center">Samantha Reds
+										<br/>
+										<small> Content manager</small></h3>
+								</div>
+								<div class="col-md-4">
+									<img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?q=75&fm=jpg&s=cfeadbd7a991e58b553bee29a7eeca55"
+										  class="center-block img-quadrata">
+									<h3 class="text-center">Samantha Reds
+										<br/>
+										<small> Content manager</small></h3>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+
+
+				<!--I want this in a box-->
+						<div class="bgimg-3">
+					<div class="section-content">
+						<h1 class="section-header"><span class="content-header wow fadeIn " data-wow-delay="0.2s"- data-wow-duration="2s"> </span></h1>
+						<h3>General questions / Project inquires</h3>
+					</div>
+					<div class="contact-section">
+						<div class="container">
+							<form>
+								<div class="col-md-6 form-line">
+									<div class="form-group">
+										<label for="exampleInputUsername">Your name</label>
+										<input type="text" class="form-control" id="" placeholder=" Enter Name">
+									</div>
+									<div class="form-group">
+										<label for="exampleInputEmail">Email Address</label>
+										<input type="email" class="form-control" id="exampleInputEmail" placeholder=" Enter Email id">
+									</div>
+									<div class="form-group">
+										<label for="telephone">Mobile No.</label>
+										<input type="tel" class="form-control" id="telephone" placeholder=" Enter 10-digit mobile no.">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="description"> Message</label>
+										<textarea class="form-control" id="description" placeholder="Enter Your Message"></textarea>
+									</div>
+									<div>
+										<button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> SendMessage</button></div>
+									<footer>
+										<div class="col-md-12">
+											<ul class="social-network social-circle">
+												<li><a href="#" class="icoGithub" title="Github"><i class="fa fa-github"></i></a>
+												</li>
+												<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+												</li>
+												<li><a href="#" class="icoInstagram" title="Google +"><i class="fa fa-instagram"></i></a></li>
+												<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+
+											<!-- reCAPTCHA -->
+											<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+											<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send
+											</button>
+											<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+
+				<script src="javascript/javascript.js"></script>
 	</body>
 </html>
