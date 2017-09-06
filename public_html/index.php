a1c90793577dd80e44ee68c56385189957e86e7c
Danielle Branch <dbranch82@gmail.com>
2017-09-06 10:08:35 -0600

    moved to website

--- public_html/index.php
+++ public_html/index.php
@@ -170,52 +170,93 @@
 						<h1 class="section-header"><span class="content-header wow fadeIn " data-wow-delay="0.2s"data-wow-duration="2s"> </span></h1>
 						<h3>General questions / Project inquires</h3>
 					</div>
-					<div class="contact-section">
-						<div class="container">
-							<form>
-								<div class="col-md-6 form-line">
-									<div class="middle">
-									<form id="contact-form" action="php/mailer.php" method="post" novalidate>
+<!--					<div class="contact-section">-->
+<!--						<div class="container">-->
+<!--							<form>-->
+<!--								<div class="col-md-6 form-line">-->
+<!--									<div class="middle">-->
+<!--									<form id="contact-form" action="php/mailer.php" method="post" novalidate>-->
+<!--									<div class="form-group">-->
+<!--										<label for="name">Your name<span class="text-danger"></span></label>-->
+<!--										<input type="text" class="form-control" id="name" placeholder=" Enter Name">-->
+<!--									</div>-->
+<!--									<div class="form-group">-->
+<!--										<label for="email">Email Address<span class="text-danger"></span></label>-->
+<!--										<input type="email" class="form-control" id="email" placeholder="Email">-->
+<!--									</div>-->
+<!---->
+<!---->
+<!--								<div class="col-md-6">-->
+<!--									<div class="form-group">-->
+<!--										<label for="message"> Message<span class="text-danger"></span></label>-->
+<!--										<textarea class="form-control" id="message" placeholder="Enter Your Message"></textarea>-->
+<!--									</div>-->
+<!--									<div>-->
+<!--									<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send-->
+<!--									</button>-->
+<!--									<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>-->
+<!--									</form>-->
+<!--										</div>-->
+								<!--Begin Contact Form-->
+								<form id="contact-form" action="php/mailer.php" method="post">
 									<div class="form-group">
-										<label for="exampleInputUsername">Your name</label>
-										<input type="text" class="form-control" id="name" placeholder=" Enter Name">
+										<label for="name">Name <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-user" aria-hidden="true"></i>
+											</div>
+											<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+										</div>
 									</div>
 									<div class="form-group">
-										<label for="exampleInputEmail">Email Address</label>
-										<input type="email" class="form-control" id="email" placeholder="Email">
+										<label for="email">Email <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-envelope" aria-hidden="true"></i>
+											</div>
+											<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+										</div>
 									</div>
-
-									
-								<div class="col-md-6">
 									<div class="form-group">
-										<label for="description"> Message</label>
-										<textarea class="form-control" id="message" placeholder="Enter Your Message"></textarea>
+										<label for="subject">Subject</label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-pencil" aria-hidden="true"></i>
+											</div>
+											<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+										</div>
 									</div>
-									<div>
-									<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send
-									</button>
-									<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-									</form>
+									<div class="form-group">
+										<label for="message">Message <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-comment" aria-hidden="true"></i>
+											</div>
+											<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
 										</div>
 
+									</div>
 									<footer>
 										<div class="col-md-12">
 											<ul class="social-network social-circle">
-												<li><a href="#" class="icoGithub" title="Github"><i class="fa fa-github"></i></a>
-												</li>
 												<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
 												</li>
 												<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
 												</li>
-												<li><a href="#" class="icoInstagram" title="Google +"><i class="fa fa-instagram"></i></a></li>
-												<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+												<li><a href="" class="icoInstagram" title="Google +"><i class="fa fa-instagram"></i></a></li>
+												<li><a href="https://www.linkedin.com/in/danielle-branch/" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
 												</li>
 											</ul>
 
 
+
 											<!-- reCAPTCHA -->
-											<div class="g-recaptcha" data-sitekey="6LeE5y4UAAAAAHWIhnNZSTKvRbQIsO7eMsns6L1l"></div>
+											<div class="g-recaptcha" data-sitekey="6Ld-rC8UAAAAAE-YzhXVwWZxNF8XyMTBZuuVuR-K"></div>
 										</div>
+
+										<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+										<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+								</form>
 											<!--empty area for form error/success output-->
 											<div class="row">
 												<div class="col-xs-12">
