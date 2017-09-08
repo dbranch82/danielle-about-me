661502a9e656bd96dd81f7762c5fffb50ea0d48d
Mason Crane <masonecrane@gmail.com>
2017-09-07 21:01:36 -0600

    squashing teh bugs

--- public_html/javascript/jquery-validate.js
+++ public_html/javascript/jquery-validate.js
@@ -1,65 +1,78 @@
 $(document).ready(function(){
 
-	/* begin validation*/
-	$("#contact-form").validate({
+    /* begin validation - call validate function on the form id */
+    $("#contact-form").validate({
 
-		// setup handling of form errors
-		debug: true,
-		errorClass: "alert alert-danger",
-		errorLabelContainer: "#output-area",
-		errorElement: "div",
+        // setup handling of form errors
+        debug: true,
+        errorClass: "alert alert-danger",
+        errorLabelContainer: "#output-area",
+        errorElement: "div",
 
-		// rules define what is good/bad input
-		// each rule starts with the form input element's NAME attribute
-		rules: {
-			name: {
-				required: true
-			},
-			email: {
-				email: true,
-				required: true
-			},
-			message: {
-				required: true,
-				maxlength: 2000
-			}
-		},
+        // rules define what is good/bad input
+        // each rule starts with the form input element's NAME attribute
+        rules: {
+            name: {
+                required: true,
+                maxlength: 20
+            },
+            email: {
+                email: true,
+                required: true,
+                maxlength: 36
+            },
+            message: {
+                required: true,
+                maxlength: 140,
+                minlength: 1
+            },
+            subject: {
+                required: false,
+                maxlength: 50
+            }
+        },
 
-		// error messages to display to the end user
-		messages: {
-			name: {
-				required: "Please enter your name."
-			},
-			email: {
-				email: "Please enter a valid email address.",
-				required: "Please enter a valid email address."
-			},
-			message: {
-				required: "Please enter a message.",
-				maxlength: "2000 characters max."
-			}
-		},
+        // error messages to display to the end user
+        messages: {
+            name: {
+                required: "Y U have no name?!",
+                maxlength: "Ur name too long!"
+            },
+            email: {
+                email: "Please enter a VALID email address.",
+                required: "Please enter a email address.",
+                maxlength: "Email is too long."
+            },
+            message: {
+                required: "Please enter a message.",
+                maxlength: "140 characters max.",
+                minlength: "Pls say something."
+            },
+            subject: {
+                maxlength: "Subject too long. Yawn."
+            }
+        },
 
-		submitHandler: function(form) {
-			$("#contact-form").ajaxSubmit({
-				type: "POST",
-				url: $("#contact-form").attr("action"),
+        submitHandler: function(form) {
+            $("#contact-form").ajaxSubmit({
+                type: "POST",
+                url: $("#contact-form").attr("action"),
 
-				success: function(ajaxOutput) {
-					// clear the output area's formatting
-					$("#output-area").css("display", "");
+                success: function(ajaxOutput) {
+                    // clear the output area's formatting
+                    $("#output-area").css("display", "");
 
-					// write the server's reply to the output area
-					$("#output-area").html(ajaxOutput);
+                    // write the server's reply to the output area
+                    $("#output-area").html(ajaxOutput);
 
-					// reset the form if it was successful
-					if($(".alert-success").length >= 1) {
-						$("#contact-form")[0].reset();
-					}
-				}
-			})
-		}
+                    // reset the form if it was successful
+                    if($(".alert-success").length >= 1) {
+                        $("#contact-form")[0].reset();
+                    }
+                }
+            })
+        }
 
-	});/* end validate function */
+    });/* end validate function */
 
 });/*end document.ready()*/
