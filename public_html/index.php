e9313c9307b35c39447298ff40475693bcb2b695
Danielle Branch <dbranch82@gmail.com>
2017-09-11 12:48:11 -0600

    made some css fixes

--- public_html/index.php
+++ public_html/index.php
@@ -34,189 +34,196 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-        <!-- jQuery Form, Additional Methods, Validate -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 
-        <!-- jQuery Validate File -->
-        <script src="javascript/jquery-validate.js"></script>
+		<!-- jQuery Validate File -->
+		<script src="javascript/jquery-validate.js"></script>
 
-        <!-- Google reCAPTCHA -->
+		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 	<body>
 
 		<!-- Begin page content -->
-			<div class="bgimg-1">
-				<nav class="navbar navbar-inverse">
-					<div class="navbar-header">
-						<a class="navbar-brand" href="">Our name</a>
-					</div>
-					<ul class="nav navbar-nav">
-						<li><a href="#services">Services</a></li>
-						<li><a href="#team">Who we are</a></li>
-						<li><a href="#contact-form">Contact</a></li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-					</ul>
-					<ul class="social-network social-circle">
-						<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
-						</li>
-						<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
-						</li>
-						<li><a href="" class="icoInstagram" title="Google +"><i class="fa fa-instagram"></i></a></li>
-						<li><a href="https://www.linkedin.com/in/danielle-branch/" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-						</li>
-					</ul>
-				</nav>
-				<div class="caption">
-					<span class="border"></span>
-					<div class="words">
+		<div class="bgimg-1">
+			<nav class="navbar navbar-inverse">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="">Our name</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li><a href="#services">Services</a></li>
+					<li><a href="#team">Who we are</a></li>
+					<li><a href="#contact-form">Contact</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				</ul>
+				<ul class="social-network social-circle">
+					<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+					</li>
+					<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+					</li>
+					<li><a href="" class="icoInstagram" title="Google +"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/danielle-branch/" class="icoLinkedin" title="Linkedin"><i
+								class="fa fa-linkedin"></i></a>
+					</li>
+				</ul>
+			</nav>
+			<div class="caption">
+				<span class="border"></span>
+				<div class="words">
 					<p>Our Story</p>
 					<p>We are an Albuquerque company specializing in whatever the hell we do.
 					</p>
 					<p>We work with Php, CSS, Bootstrap, Javascript, and Wordpress. </p>
 					<p>We partner with other artisans in the albuquerque area to help you vision become a reality.</p>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="services" class="container">
+
+			<div class="row form-group product-chooser">
+				<h2>What we provide</h2>
+
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="product-chooser-item">
+						<img src="images/css.jpg"
+							  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile and Desktop">
+						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+
+							<span class="title">CSS fixes</span>
+							<span class="description">Regardless of the issue whether it is a font size, color or spacing issue we will look at your css issue and repair it for a one time fee</span>
+							<input type="radio" name="product" value="mobile_desktop" checked="checked">
+						</div>
+						<div class="clear"></div>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="product-chooser-item">
+						<img src="images/wordpress.jpg"
+							  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Desktop">
+						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+							<span class="title">Wordpress</span>
+							<span class="description">We can build you a word press site using prebuilt themes using wordpresses built in scalibkility and customization. We can install, customize and maintain your back end. </span>
+							<input type="radio" name="product" value="desktop">
+						</div>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="product-chooser-item">
+						<img src="images/fullwebdesign.png"
+							  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile">
+						<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+							<span class="title">Fully responsive websites</span>
+							<span class="description">We can create fully responsive websites to meet all of your personal and business needs. We will build it from the floor up and make it accessible to mobile users as well</span>
+							<input type="radio" name="product" value="mobile">
+						</div>
+						<div class="clear"></div>
 					</div>
 				</div>
 			</div>
-
-
-			<div id="services" class="container">
-
-				<div class="row form-group product-chooser">
-					<h2>What we provide</h2>
-
-					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="product-chooser-item">
-							<img src="images/css.jpg"
-								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile and Desktop">
-							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-
-								<span class="title">CSS fixes</span>
-								<span class="description">Regardless of the issue whether it is a font size, color or spacing issue we will look at your css issue and repair it for a one time fee</span>
-								<input type="radio" name="product" value="mobile_desktop" checked="checked">
-							</div>
-							<div class="clear"></div>
-						</div>
+		</div>
+		<!---us--->
+		<div class="section">
+			<div class="container">
+				<div id="team" class="row">
+					<div class="col-md-12">
+						<h1 class="text-center text-primary">Meet our Team</h1>
+						<p class="text-center">We are a group of professionals that use our individual skill sets to deliver
+							polished, efficiant websites </p>
 					</div>
+				</div>
+				<div class="row">
 
-					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="product-chooser-item">
-							<img src="images/wordpress.jpg"
-								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Desktop">
-							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-								<span class="title">Wordpress</span>
-								<span class="description">We can build you a word press site using prebuilt themes using wordpresses built in scalibkility and customization. We can install, customize and maintain your back end. </span>
-								<input type="radio" name="product" value="desktop">
-							</div>
-							<div class="clear"></div>
-						</div>
+
+					<div class="col-md-4">
+						<img src="images/michelle.jpg"
+							  class="img-quadrata center-block">
+						<h3 class="text-center">Michelle Allen
+							<br/>
+							<small> Web Designer/VP Sales</small>
+						</h3>
 					</div>
-					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="product-chooser-item">
-							<img src="images/fullwebdesign.png"
-								  class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile">
-							<div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
-								<span class="title">Fully responsive websites</span>
-								<span class="description">We can create fully responsive websites to meet all of your personal and business needs. We will build it from the floor up and make it accessible to mobile users as well</span>
-								<input type="radio" name="product" value="mobile">
-							</div>
-							<div class="clear"></div>
-						</div>
+					<div class="col-md-4">
+						<img src="images/myface.jpg"
+							  class="center-block img-quadrata">
+						<h3 class="text-center">Danielle Branch
+							<br/>
+							<small> Web Designer/VP Networking</small>
+						</h3>
 					</div>
 				</div>
 			</div>
-<!---us--->
-					<div class="section">
-						<div class="container">
-							<div id="team" class="row">
-								<div class="col-md-12">
-									<h1 class="text-center text-primary">Meet our Team</h1>
-									<p class="text-center">We are a group of professionals that use our individual skill sets to deliver polished, efficiant websites </p>
-								</div>
-							</div>
-							<div class="row">
+		</div>
 
 
-								<div class="col-md-4">
-									<img src="images/michelle.jpg"
-										  class="img-quadrata center-block" >
-									<h3 class="text-center" >Michelle Allen
-										<br/>
-										<small> Web Designer/VP Sales</small></h3>
-								</div>
-								<div class="col-md-4">
-									<img src="images/myface.jpg"
-										  class="center-block img-quadrata" >
-									<h3 class="text-center" >Danielle Branch
-										<br/>
-										<small> Web Designer/VP Networking</small></h3>
-								</div>
-							</div>
+		<!--I want this in a box-->
+		<div class="bgimg-3">
+			<!--Begin Contact Form-->
+
+			<form id="contact-form" action="./php/mailer.php" method="post" novalidate>
+				<div class="form-group">
+					<label for="name">Name <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-user" aria-hidden="true"></i>
 						</div>
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 					</div>
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="message">Message <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea class="form-control" rows="5" id="message" name="message"
+									 placeholder="Message (2000 characters max)"></textarea>
+					</div>
+				</div>
 
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6Ld-rC8UAAAAAE-YzhXVwWZxNF8XyMTBZuuVuR-K"></div>
 
+				<div class="buttons">
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</div>
+			</form>
 
-
-				<!--I want this in a box-->
-						<div class="bgimg-3">
-								<!--Begin Contact Form-->
-
-								<form id="contact-form" action="./php/mailer.php" method="post" novalidate>
-									<div class="form-group">
-										<label for="name">Name <span class="text-danger">*</span></label>
-										<div class="input-group">
-											<div class="input-group-addon">
-												<i class="fa fa-user" aria-hidden="true"></i>
-											</div>
-											<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="email">Email <span class="text-danger">*</span></label>
-										<div class="input-group">
-											<div class="input-group-addon">
-												<i class="fa fa-envelope" aria-hidden="true"></i>
-											</div>
-											<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="subject">Subject</label>
-										<div class="input-group">
-											<div class="input-group-addon">
-												<i class="fa fa-pencil" aria-hidden="true"></i>
-											</div>
-											<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="message">Message <span class="text-danger">*</span></label>
-										<div class="input-group">
-											<div class="input-group-addon">
-												<i class="fa fa-comment" aria-hidden="true"></i>
-											</div>
-											<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
-										</div>
-									</div>
-
-											<!-- reCAPTCHA -->
-											<div class="g-recaptcha" data-sitekey="6Ld-rC8UAAAAAE-YzhXVwWZxNF8XyMTBZuuVuR-K"></div>
-
-<div class="buttons">
-										<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-										<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-</div>
-								</form>
-
-<!--								<div class="col-md-12">-->
-							<div class="row">
-								<div class="col-xs-12">
-									<div id="output-area">
-									</div>
-								</div>
+			<!--								<div class="col-md-12">-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div id="output-area">
+					</div>
+				</div>
+			</div>
 	</body>
 </html>
